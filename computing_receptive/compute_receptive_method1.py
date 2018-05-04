c1fe347d306a7a124e0691fc1c2a92e21a7b6b0c
14zwyan <532707544@qq.com>
2018-05-04 18:55:51 +0800

    compute receptive field for ssd300

--- computing_receptive/compute_receptive_method1.py
+++ computing_receptive/compute_receptive_method1.py
@@ -15,7 +15,21 @@ net_struct = {'alexnet': {'net':[[11,4,0],[3,2,0],[5,1,2],[3,2,0],[3,1,1],[3,1,1
        'zf-5':{'net': [[7,2,3],[3,2,1],[5,2,2],[3,2,1],[3,1,1],[3,1,1],[3,1,1]],
                'name': ['conv1','pool1','conv2','pool2','conv3','conv4','conv5']}}
 
-imsize=224
+
+
+net_struct1= {
+                'ssd_300':{
+                    'net':[[3,1,1],[3,1,1],[2,2,0],[3,1,1],[3,1,1],[2,2,0],[3,1,1],[3,1,1],[3,1,1],
+                                     [2,2,0],[3,1,1],[3,1,1],[3,1,1],[2,2,0],[3,1,1],[3,1,1],[3,1,1],[2,2,0],[3,1,6],
+                                     [1,1,0],[1,1,0],[3,2,1],[1,1,0],[3,2,1],[1,1,0],[3,1,0],[1,1,0],[3,1,0]],
+                    'name':['conv1_1','conv1_2','pool1','conv2_1','conv2_2','pool2','conv3_1','conv3_2',
+                            'conv3_3','pool3','conv4_1','conv4_2','conv4_3','pool4','conv5_1','conv5_2','conv5_3','pool5','fc6',
+                            'fc7','conv6_1','conv6_2','conv7_1','conv7_2','conv8_1','conv8_2','conv9_1','conv9_2']
+                },
+            }
+
+
+imsize=300
 
 def outFromIn(imsiz,net,layernum):
     tostride=1
@@ -38,9 +52,9 @@ if __name__=='__main__':
     print "layer output sizes given image = %dx%d" % (imsize, imsize)
 
     p=()
-    for net in net_struct.keys():
+    for net in net_struct1.keys():
          print '************net structrue name is %s**************'% net
-         for i in range( len( net_struct[ net ]['net'] )):
-            p=outFromIn(imsize,net_struct[ net ][ 'net'],i+1)
-            rf=inFromOut( net_struct[net][ 'net' ],i+1)
-            print "Layer Name = %s, Output size = %3d, Stride = % 3d, RF size = %3d" % (net_struct[net]['name'][i], p[0], p[1], rf)
+         for i in range( len( net_struct1[ net ]['net'] )):
+            p=outFromIn(imsize,net_struct1[ net ][ 'net'],i+1)
+            rf=inFromOut( net_struct1[net][ 'net' ],i+1)
+            print "Layer Name = %s, Output size = %3d, Stride = % 3d, RF size = %3d" % (net_struct1[net]['name'][i], p[0], p[1], rf)
