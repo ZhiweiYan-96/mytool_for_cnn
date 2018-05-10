9baadc96d852fccdefa4b62e2e4a80c4a599e0f5
14zwyan <532707544@qq.com>
2018-05-10 11:39:40 +0800

    new result on fusion

--- compare_mAP/compare_ap.py
+++ compare_mAP/compare_ap.py
@@ -1,5 +1,6 @@
 import numpy
 import matplotlib.pyplot as plt 
+import argparse
 
 
 def parse_file(file_name):
@@ -18,9 +19,23 @@ def parse_file(file_name):
 	print(aps)
 	return names,aps,meanAP
 
-
+parser=argparse.ArgumentParser(description="Script that compare aps of two model")
+parser.add_argument('--file1',type=str,help="file 1")
+parser.add_argument('--file2',type=str,help="file 2")
+args=parser.parse_args()
 if __name__=="__main__":
-	(wide_name,wide_aps,wide_mAP)=parse_file('wide_ap.txt')
+
+	file1=args.file1
+	file2=args.file2
+	(file1_names,file1_aps,file1_mAP)=parse_file(file1)
+	(file2_names,file2_aps,file2_mAP)=parse_file(file2)
+	plt.plot(file1_names,file1_aps,label=file1+'_'+str(file1_mAP))
+	plt.plot(file2_names,file2_aps,label=file2+'_'+str(file2_mAP))
+	plt.legend()
+	plt.show()
+
+	'''
+	(wide_name,wide_aps,wide_mAP)=parse_file(i)
 	(baseline_name,baseline_aps,baseline_mAP)=parse_file('baseline_ap.txt')
 	(deep_name,deep_aps,deep_mAP)=parse_file('deep_ap.txt')
 	#plt.plot(wide_name,wide_aps,label='wide_'+str(wide_mAP))
@@ -29,3 +44,4 @@ if __name__=="__main__":
 	
 	plt.legend()
 	plt.show()
+	'''
