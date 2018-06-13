783d9c2e005da376fc02e4ea53a6ee65e09c8a55
14zwyan <532707544@qq.com>
2018-06-14 07:41:22 +0800

    Squeeze and Exitation sturcutre for SSD

---
+++ toGPU/__init__.py
@@ -0,0 +1,6 @@
+from .l2norm import L2Norm
+from .multibox_loss import MultiBoxLoss
+from .generalize_pool import Generalize_Pool2d
+from .se_module import SELayer
+
+__all__ = ['L2Norm', 'MultiBoxLoss','SELayer']
